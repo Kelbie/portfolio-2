e3b22aa24752fcbdd5a4f319d582a24c322cdd35
Kevin Kelbie <notkelbie@outlook.com>
2020-05-14 07:11:45 +0100

    add svg fixed border

--- src/App.jsx
+++ src/App.jsx
@@ -11,8 +11,32 @@ function Section({className, label, children, ...props}) {
   </section>;
 }
 
+function Border() {
+  return <div className="border">
+    <svg>
+    <defs>
+      <linearGradient id="linear" x1="0%" y1="0%" x2="0%" y2="100%">
+        <stop offset="0%" stopColor="#f6d371" />
+        <stop offset="10%" stopColor="#f27a21" />
+        <stop offset="50%" stopColor="#cf2872" />
+        <stop offset="90%" stopColor=" #912eb9" />
+        <stop offset="100%" stopColor="#4d58ce" />
+      </linearGradient>
+    </defs>
+    <rect x="0"
+      y="0"
+      width="100%"
+      height="100%"
+      stroke="url(#linear)"
+      strokeWidth="0.4em"
+      fill="none" />
+    </svg>
+  </div>;
+}
+
 export default function App() {
   return <div className="App">
+      <Border />
       <h1>Kevin Kelbie</h1>
       <Section label={'projects'}>
         <Project title="title"
