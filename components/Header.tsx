09848f21a6b54b6f9a629deebd9da61164728637
Kevin Kelbie <notkelbie@outlook.com>
2020-05-24 00:06:51 +0100

    add linkedin

--- components/Header.tsx
+++ components/Header.tsx
@@ -7,7 +7,7 @@ import Gradient from './Gradient';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBookOpen, faBriefcase, faRss, faCode, faInfo, faInfoCircle, faLink } from "@fortawesome/free-solid-svg-icons";
-import { faOsi, faDev, faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faOsi, faDev, faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 export interface Props {
   className?: string;
@@ -17,6 +17,9 @@ function Header({className}: Props) {
   return <Gradient className={className} side={"top"}>
     <h1>Kevin Kelbie</h1>
     <div className="links">
+      <a href={"https://www.linkedin.com/in/kelbie/"}>
+        <FontAwesomeIcon  icon={faLinkedin} />
+      </a>
       <a href={"https://github.com/KevinKelbie/"}>
         <FontAwesomeIcon  icon={faGithub} />
       </a>
@@ -48,12 +51,14 @@ export default styled(Header)`
   }
 
   .links {
+    display: flex;
     height: max-content;
     align-self: center;
     padding-right: max(32px, calc(50vw - 800px / 2));
   }
 
   a {
+    margin-left: 16px;
     height: max-content;
     font-size: 24px;
   }
