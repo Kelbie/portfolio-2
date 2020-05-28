a856367198c89defc067822936ad51f95d428708
Kevin Kelbie <notkelbie@outlook.com>
2020-05-28 14:39:18 +0100

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -404,14 +404,15 @@ export default styled(Index)`
   .horizontal {
     position: relative;
     width: 100vw;
-    overflow: scroll;
     /* 
+    overflow: scroll;
     overflow-y: hidden;
+    */
     left: 50%;
     right: 50%;
     margin-left: -50vw;
     margin-right: -50vw;
-    white-space: nowrap; */
+    white-space: nowrap; 
     
     > div {
       display: inline-block;
