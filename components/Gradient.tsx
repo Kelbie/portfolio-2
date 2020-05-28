f1893097e857d5f7e4faabe252263f61aa772e33
Kevin Kelbie <notkelbie@outlook.com>
2020-05-28 03:51:47 +0100

    Update Gradient.tsx

--- components/Gradient.tsx
+++ components/Gradient.tsx
@@ -25,14 +25,14 @@ export default styled(Gradient)`
 				position: fixed;
 				width: max(32px, calc(50vw - 800px / 2));
         left: 4px;
-				background-image: linear-gradient(to right, hsl(210, 34%, 13%), hsla(210, 34%, 13%, 0));
+				background-image: linear-gradient(to right, hsl(211, 35%, 13%), hsla(211, 35%, 13%, 0));
 				height: 100vh;
     }
     &.right {
 				position: fixed;
 				width: max(32px, calc(50vw - 800px / 2));
         transform: rotate(180deg);
-				background-image: linear-gradient(to right, hsl(210, 34%, 13%), hsla(210, 34%, 13%, 0));
+				background-image: linear-gradient(to right, hsl(211, 35%, 13%), hsla(211, 35%, 13%, 0));
         right: 4px;
 				height: 100vh;
     }
@@ -41,6 +41,6 @@ export default styled(Gradient)`
 			left: 0;
 			height: 100px;
 			top: 0px;
-			background-image: linear-gradient(to bottom, hsl(210, 34%, 13%), hsla(210, 34%, 13%, 1), hsla(210, 34%, 13%,0.5));
+			background-image: linear-gradient(to bottom, hsl(211, 35%, 13%), hsla(211, 35%, 13%, 1), hsla(211, 35%, 13%,0.5));
 		}
 `;
