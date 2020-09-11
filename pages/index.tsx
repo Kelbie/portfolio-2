3803a3b1862cb7ec7eea07d50980c80dca7b6ef5
Kevin Kelbie <notkelbie@outlook.com>
2020-09-11 04:14:57 +0100

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -52,7 +52,7 @@ const projects: Project[] = [
     }
   },
   {
-    title: "Korean Typing Game",
+    title: "Hangul.cool: Korean Typing Game",
     img: "/Korean.png",
     video: "https://youtu.be/uxMzXLikebs",
     description: `A Korean Typing Game designed to help people learn how to type in Korean by breaking down the multi-syllabic blocks into individual syllables to make them easier to identify which cannot be done with unicode.`,
